267a7eaff5c651f4714f3fe46b3e86c8b7996181
Benjamin <loco.queens@gmail.com>
2021-09-07 21:47:54 -0700

    added main game mechanics

---
+++ Tilted/Assets/Scripts/Orb.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orb : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody orbRB = GetComponent<Rigidbody>();
+        orbRB.AddForce(Physics.gravity * orbRB.mass);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.name == "Goal")
+        {
+            Destroy(gameObject, .2f);
+            Debug.Log("You Win");
+        }
+    }
+}
---
+++ Tilted/Assets/Scripts/Tilt.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilt : MonoBehaviour
+{
+    public Vector3 currentRotation;
+    public Quaternion startQuaternion;
+    public float lerpTime = 1f;
+    public float rotateAmount = .1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startQuaternion = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        currentRotation = GetComponent<Transform>().eulerAngles;
+
+        if ((Input.GetKey(KeyCode.LeftArrow)) && (currentRotation.z <= 10 || currentRotation.z >= 348))
+        {
+            transform.Rotate(Vector3.forward * rotateAmount);
+        }
+        if ((Input.GetKey(KeyCode.RightArrow)) && (currentRotation.z >= 349 || currentRotation.z <= 11))
+        {
+            transform.Rotate(Vector3.back * rotateAmount);
+        }
+        if ((Input.GetKey(KeyCode.UpArrow)) && (currentRotation.x <= 10 || currentRotation.x >= 348))
+        {
+            transform.Rotate(Vector3.right * rotateAmount);
+        }
+        if ((Input.GetKey(KeyCode.DownArrow)) && (currentRotation.x >= 349 || currentRotation.x <= 11))
+        {
+            transform.Rotate(Vector3.left * rotateAmount);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, startQuaternion, Time.deltaTime * lerpTime);
+        }
+    }
+
+}
+
+
+//if ((Input.GetKey(KeyCode.LeftArrow)) && (currentRotation.z <= 10 || currentRotation.z >= 348))
+//{
+//    transform.Rotate(0, 0, 1);
+//}
+//if ((Input.GetKey(KeyCode.RightArrow)) && (currentRotation.z >= 349 || currentRotation.z <= 11))
+//{
+//    transform.Rotate(0, 0, -1);
+//}
+//if ((Input.GetKey(KeyCode.UpArrow)) && (currentRotation.x <= 10 || currentRotation.x >= 348))
+//{
+//    transform.Rotate(1, 0, 0);
+//}
+//if ((Input.GetKey(KeyCode.DownArrow)) && (currentRotation.x >= 349 || currentRotation.x <= 11))
+//{
+//    transform.Rotate(-1, 0, 0);
+//}
